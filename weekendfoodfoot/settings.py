467d4b9936534854bd93e18c3abff745b3b2bbb3
Aubin Koenigssohn <mvogtsinga@gmail.com>
2024-06-24 17:24:38 +0000

    Setup database and cloudinary

--- weekendfoodfoot/settings.py
+++ weekendfoodfoot/settings.py
@@ -13,15 +13,20 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
+if os.path.isfile("env.py"):
+    import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i_y(s+ca5$)-n0kzfh#p9$3$#o0tpx)u$%x+(sm-hy)59on=sq'
+# SECRET_KEY = 'django-insecure-i_y(s+ca5$)-n0kzfh#p9$3$#o0tpx)u$%x+(sm-hy)59on=sq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
