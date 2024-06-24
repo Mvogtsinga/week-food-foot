9cf89def96038626c4bb9459c08e8a219c6be886
Aubin Koenigssohn <mvogtsinga@gmail.com>
2024-06-24 14:58:58 +0000

    Setup SMTP.gmail.com

--- weekendfoodfoot/settings.py
+++ weekendfoodfoot/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'votre.adresse@gmail.com'
-EMAIL_HOST_PASSWORD = 'votre_mot_de_passe'
+EMAIL_HOST_USER = 'mvogtsing@gmail.com'
+EMAIL_HOST_PASSWORD = 'AKtreize13'
